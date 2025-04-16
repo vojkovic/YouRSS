c150dc0e5b480d3f016e5301721a335f1ebb23c3
vojkovic <brockv@tuta.io>
2025-04-16 23:01:41 +0800

    feat: version 1.0

--- main.go
+++ main.go
@@ -24,14 +24,17 @@ type Feed struct {
 }
 
 type Entry struct {
-    Title      string    `xml:"title"`
-    Link       Link      `xml:"link"`
-    Published  string    `xml:"published"`
-    PubDate    time.Time `xml:"-"`
-    Author     string    `xml:"author>name"`
-    Thumbnail  string      `xml:"-"`
-    Views      string    `xml:"-"`
-    MediaGroup MediaGroup `xml:"group"`
+    Title         string    `xml:"title"`
+    Link          Link      `xml:"link"`
+    Published     string    `xml:"published"`
+    Updated       string    `xml:"updated"`
+    PubDate       time.Time `xml:"-"`
+    UpdateDate    time.Time `xml:"-"`
+    Author        string    `xml:"author>name"`
+    Thumbnail     string    `xml:"-"`
+    Views         string    `xml:"-"`
+    VideoID       string    `xml:"videoId"`
+    MediaGroup    MediaGroup `xml:"group"`
 }
 
 type Link struct {
@@ -39,8 +42,9 @@ type Link struct {
 }
 
 type MediaGroup struct {
-    Thumbnail Thumbnail `xml:"thumbnail"`
-    MediaCommunity MediaCommunity  `xml:"community"`
+    Thumbnail      Thumbnail      `xml:"thumbnail"`
+    MediaCommunity MediaCommunity `xml:"community"`
+    Description    string         `xml:"description"`
 }
 
 type Thumbnail struct {
@@ -49,10 +53,16 @@ type Thumbnail struct {
 
 type MediaCommunity struct {
     Statistics Statistics `xml:"statistics"`
+    Rating     Rating    `xml:"starRating"`
 }
 
 type Statistics struct {
     Views string `xml:"views,attr"`
+}
+
+type Rating struct {
+    Count   string `xml:"count,attr"`
+    Average string `xml:"average,attr"`
 }
 
 var (
@@ -70,6 +80,8 @@ func main() {
     tmpl = template.Must(template.New("feed.html").Funcs(template.FuncMap{
         "timeAgo": timeAgo,
         "viewCount": viewCount,
+        "truncate": truncateText,
+        "likeCount": likeCount,
     }).ParseFiles("templates/feed.html"))
 
     go updateFeeds(cfg, 5*time.Minute)
@@ -99,14 +111,17 @@ func loadConfig(path string) (Config, error) {
 }
 
 func fetchFeed(url string) ([]Entry, error) {
+    log.Printf("Fetching feed from: %s", url)
     resp, err := http.Get(url)
     if err != nil {
+        log.Printf("Error fetching feed: %v", err)
         return nil, err
     }
     defer resp.Body.Close()
 
     body, err := io.ReadAll(resp.Body)
     if err != nil {
+        log.Printf("Error reading response body: %v", err)
         return nil, err
     }
 
@@ -114,11 +129,17 @@ func fetchFeed(url string) ([]Entry, error) {
 
     var feed Feed
     if err := xml.NewDecoder(resp.Body).Decode(&feed); err != nil {
+        log.Printf("Error decoding XML: %v", err)
         return nil, err
     }
 
+    log.Printf("Found %d entries in feed", len(feed.Entries))
+
     for i := range feed.Entries {
         feed.Entries[i].PubDate, _ = time.Parse(time.RFC3339, feed.Entries[i].Published)
+        if feed.Entries[i].Updated != "" {
+            feed.Entries[i].UpdateDate, _ = time.Parse(time.RFC3339, feed.Entries[i].Updated)
+        }
         feed.Entries[i].Thumbnail = feed.Entries[i].MediaGroup.Thumbnail.URL
         feed.Entries[i].Views = feed.Entries[i].MediaGroup.MediaCommunity.Statistics.Views
     }
@@ -128,14 +149,19 @@ func fetchFeed(url string) ([]Entry, error) {
 
 func updateFeeds(cfg Config, interval time.Duration) {
     for {
+        log.Printf("Starting feed update...")
         var newEntries []Entry
-        for _, url := range cfg.Channels {
+        for name, url := range cfg.Channels {
+            log.Printf("Fetching channel %s...", name)
             entries, err := fetchFeed("https://www.youtube.com/feeds/videos.xml?channel_id=" + url)
             if err != nil {
+                log.Printf("Error fetching channel %s: %v", name, err)
                 continue
             }
             newEntries = append(newEntries, entries...)
         }
+
+        log.Printf("Total entries fetched: %d", len(newEntries))
 
         sort.Slice(newEntries, func(i, j int) bool {
             return newEntries[i].PubDate.After(newEntries[j].PubDate)
@@ -145,6 +171,7 @@ func updateFeeds(cfg Config, interval time.Duration) {
         allEntries = newEntries
         feedsMutex.Unlock()
 
+        log.Printf("Feed update complete. Next update in %v", interval)
         time.Sleep(interval)
     }
 }
@@ -171,7 +198,7 @@ func feedHandler(w http.ResponseWriter, r *http.Request) {
 
 func timeAgo(t time.Time) string {
     diff := time.Since(t)
-
+    
     switch {
     case diff < time.Minute:
         return "just now"
@@ -185,12 +212,20 @@ func timeAgo(t time.Time) string {
             return "1 hour ago"
         }
         return fmt.Sprintf("%d hours ago", int(diff.Hours()))
-    default:
+    case diff < 30*24*time.Hour:
         days := int(diff.Hours() / 24)
         if days == 1 {
             return "1 day ago"
         }
         return fmt.Sprintf("%d days ago", days)
+    case diff < 365*24*time.Hour:
+        months := int(diff.Hours() / (24 * 30))
+        if months == 1 {
+            return "1 month ago"
+        }
+        return fmt.Sprintf("%d months ago", months)
+    default:
+        return t.Format("Jan 2, 2006")
     }
 }
 
@@ -208,5 +243,29 @@ func viewCount(views string) string {
         return fmt.Sprintf("%.1fM views", float64(viewsInt)/1000000)
     default:
         return fmt.Sprintf("%.0fM views", float64(viewsInt)/1000000)
+    }
+}
+
+func truncateText(text string, length int) string {
+    if len(text) <= length {
+        return text
+    }
+    return text[:length] + "..."
+}
+
+func likeCount(likes string) string {
+    likesInt := 0
+    fmt.Sscanf(likes, "%d", &likesInt)
+    switch {
+    case likesInt < 1000:
+        return fmt.Sprintf("%d", likesInt)
+    case likesInt < 10000:
+        return fmt.Sprintf("%.1fK", float64(likesInt)/1000)
+    case likesInt < 1000000:
+        return fmt.Sprintf("%.0fK", float64(likesInt)/1000)
+    case likesInt < 10000000:
+        return fmt.Sprintf("%.1fM", float64(likesInt)/1000000)
+    default:
+        return fmt.Sprintf("%.0fM", float64(likesInt)/1000000)
     }
 }
